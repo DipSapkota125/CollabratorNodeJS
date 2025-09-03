fd1b60ae0a27619f4fcaa3abf581336f4452446f
DipSapkota125 <dipsapkota325@gmail.com>
2025-09-03 13:11:35 +0545

    added nodejs backend folder

---
+++ README.md
@@ -0,0 +1,16 @@
+# Node.js Server with Dotenv
+
+This is a Node.js server project that uses **dotenv** to manage environment variables. It demonstrates how to securely configure environment-specific settings, including database URLs, ports, and API keys.
+
+## Features
+
+- Easy setup for environment variables using `dotenv`
+- Node.js server ready to run locally or in production
+- Secure management of sensitive credentials
+- Works with npm 10.x and 11.x
+
+## Installation
+
+1. **Clone the repository**
+   git clone: https://github.com/DipSapkota125/CollabratorNodeJS.git
+   cd your-repo
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import os from "os";
+import app from "./app";
+import connectDB from "./config/db";
+require("dotenv").config();
+
+const PORT = Number(process.env.PORT || 5000);
+
+//DB Connection
+connectDB();
+
+const networkInterfaces = os.networkInterfaces();
+let localIP = "127.0.0.1";
+
+for (const ifaceList of Object.values(networkInterfaces)) {
+  if (!ifaceList) continue;
+  for (const iface of ifaceList) {
+    if (iface.family === "IPv4" && !iface.internal) {
+      localIP = iface.address;
+      break;
+    }
+  }
+  if (localIP !== "127.0.0.1") break;
+}
+
+// Start server
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`📌 Server running`);
+  console.log(`   - Localhost: http://localhost:${PORT}`);
+  console.log(`   - Network:   http://${localIP}:${PORT}`);
+  console.log("===========================================");
+});
